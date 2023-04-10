2a9c1dacd5dec75082ccd767b42ba5c07a3b6756
Sandeep Reddy <sandeep.thippareddy.ext@zeiss.com>
2023-04-10 17:12:57 +0530

    Add project files.

---
+++ CUstomEventGridTopicEventPublisher/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEventGridTopicEventPublisher
+{
+    internal class Order
+    {
+        public int OrderId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quality { get; set; }
+    }
+}
---
+++ CUstomEventGridTopicEventPublisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using Azure;
+using Azure.Messaging.EventGrid;
+using CustomEventGridTopicEventPublisher;
+using System.Text.Json;
+
+var customTopicEndpoint = new Uri("https://customeventgridtopic.eastus-1.eventgrid.azure.net/api/events");
+var accessKeyCred = new AzureKeyCredential("jQJUmJGBSoRvxnkaTetJx/JzphzpAALVOHb1GYfzi+M=");
+
+
+EventGridPublisherClient publisherClient = new EventGridPublisherClient(customTopicEndpoint, accessKeyCred);
+
+var order = new Order()
+{
+    OrderId= 1,
+    Quality = 10,
+    UnitPrice = 9.8M
+};
+
+List<EventGridEvent> eventGridEvents = new List<EventGridEvent>()
+{
+    new EventGridEvent("Posting a New Order", "app.neworder", "1.0", JsonSerializer.Serialize(order))
+};
+
+
+publisherClient.SendEvents(eventGridEvents);
+
+Console.WriteLine("Events Sent to Custom Topic");
